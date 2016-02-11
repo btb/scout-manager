084859d800a5dbc4b001a08bb3c6f07761e7936a
jcivjan <jcivjan@uw.edu>
2016-02-11 08:06:07 -0800

    getting space list working

--- scout_manager/urls.py
+++ scout_manager/urls.py
@@ -5,13 +5,12 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    # Examples:
-
-    url(r'^$', 'scout_manager.views.list', name='list'),
+    # application urls
     url(r'^add/', 'scout_manager.views.add', name='add'),
     url(r'^$item/', 'scout_manager.views.item', name='item'),
     url(r'^$publish/', 'scout_manager.views.publish', name='publish'),
     url(r'^space/', 'scout_manager.views.space', name='space'),
     url(r'^test/', 'scout_manager.views.test', name='test'),
+    url(r'^$', 'scout_manager.views.list', name='list'),
 
 )
--- scout_manager/views.py
+++ scout_manager/views.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from htmlmin.decorators import minified_response
+from scout.space_dao import get_spot_list, get_spot_by_id
 
 # Create your views here.
 
 def list(request):
-    return render_to_response(
-        'scout_manager/list.html',
-        context_instance=RequestContext(request))
+    spots = get_spot_list()
+    context = {"spots": spots}
+    return render_to_response('scout_manager/list.html', context,
+                              context_instance=RequestContext(request))
 
 
 def add(request):
