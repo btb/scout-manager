db527c4243720eed053ec0db895eab6b9f66e228
Charlon Palacay <charlon@gmail.com>
2016-04-28 09:17:24 -0700

    Updates urls patters for Django 1.9

--- scout_manager/urls.py
+++ scout_manager/urls.py
@@ -1,28 +1,29 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from scout_manager.views.api import Spot
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+from scout_manager.views import pages
+
+urlpatterns = [
     # Examples:
 
     # /manager/
-    url(r'^$', 'scout_manager.views.pages.home', name='home'),
+    url(r'^$', pages.home, name='home'),
 
     # /items/
-    url(r'^items/$', 'scout_manager.views.pages.items', name='items'),
-    url(r'^items/(?P<item_id>[0-9]{1,5})', 'scout_manager.views.pages.items_edit', name='items_edit'),
-    url(r'^items/add/', 'scout_manager.views.items_add', name='items_add'),
+    url(r'^items/$', pages.items, name='items'),
+    url(r'^items/(?P<item_id>[0-9]{1,5})', pages.items_edit,
+        name='items_edit'),
+    url(r'^items/add/', pages.items_add, name='items_add'),
 
     # /spaces/
-    url(r'^spaces/$', 'scout_manager.views.pages.spaces', name='spaces'),
-    url(r'^spaces/(?P<spot_id>[0-9]{1,5})', 'scout_manager.views.pages.spaces_edit', name='spaces_edit'),
-    url(r'^spaces/add/', 'scout_manager.views.pages.spaces_add', name='spaces_add'),
-
-    url(r'^schedule/(?P<spot_id>[0-9]{1,5})', 'scout_manager.views.pages.schedule', name='schedule'),
-
+    url(r'^spaces/$', pages.spaces, name='spaces'),
+    url(r'^spaces/(?P<spot_id>[0-9]{1,5})', pages.spaces_edit,
+        name='spaces_edit'),
+    url(r'^spaces/add/', pages.spaces_add, name='spaces_add'),
+    url(r'^schedule/(?P<spot_id>[0-9]{1,5})', pages.schedule,
+        name='schedule'),
     url(r'api/spot/(?P<spot_id>[0-9]{1,5})', Spot().run)
 
-
-)
+]
