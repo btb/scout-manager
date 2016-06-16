b968fa9dbc1049a175c39d9169e4250d36f5f6b5
Kevin <zhaok24@uw.edu>
2016-06-16 17:28:34 +0000

    figured out Cookies, need fidelity to finish to know what to test for though...

--- scout_manager/static/scout_manager/js/test/spot_test.js
+++ scout_manager/static/scout_manager/js/test/spot_test.js
@@ -10,6 +10,8 @@ var jqueryFromHtml = function jqueryFromHtml(html) {
     var win = doc.parentWindow;
     var $ = jquery(win);
     // Function to serialize form data into an JS object
+    // (Takes html from the form and sorts it into a 
+    // valid dictionary
     $.fn.serializeObject = function() {
         var o = {};
         var a = this.serializeArray();
@@ -30,33 +32,41 @@ var jqueryFromHtml = function jqueryFromHtml(html) {
 
 describe("Spot Tests", function() {
     describe("Submit Spot", function() {
-        describe("Create Spot", function() {       
+        describe("Create Spot", function() { 
+            beforeEach(function() {
+                global.Cookies = Cookies;
+                // By not having an id in the html, the program should recognize to CREATE
+                global.$ = jqueryFromHtml('<form> <div class="alert hidden" id="pub_error"> </div>');
+            });
             it('should alert that there was a success', function() {
-                global.$ = jqueryFromHtml('<form> <div class="alert hidden" id="pub_error"> </div> </form>');
+                // to be completed (figure out how to get a valid spot)
                 Spot.submit_spot();
                 var error = $("#pub_error");
                 var classes = error.attr("class");
                 assert.equal(classes, 'alert alert-success');
             });
             it('should alert that there was a failure', function() {
-                global.$ = jqueryFromHtml('<form> <div class="alert hidden" id="pub_error"> </div> </form>');
                 Spot.submit_spot();
-                assert.equal();
                 var error = $("#pub_error");
                 var classes = error.attr("class");
                 assert.equal(classes, 'alert alert-danger');
             });
         });
         describe("Edit Spot", function() {       
+            beforeEach(function() {
+                global.Cookies = Cookies;
+                // By having an hidden id in the html, the program should recognize to EDIT
+                global.$ = jqueryFromHtml('<form> <div class="alert hidden" id="pub_error"> </div>' + 
+                                          '<input name="id" type="hidden" value="1"> </form>');
+            });
             it('should alert that there was a success', function() {
-                global.$ = jqueryFromHtml('<form> <div class="alert hidden" id="pub_error"> </div> </form>');
+                // to be completed (figure out how to get a valid spot)
                 Spot.submit_spot();
                 var error = $("#pub_error");
                 var classes = error.attr("class");
                 assert.equal(classes, 'alert alert-success');
             });
             it('should alert that there was a failure', function() {
-                global.$ = jqueryFromHtml('<form> <div class="alert hidden" id="pub_error"> </div> </form>');
                 Spot.submit_spot();
                 var error = $("#pub_error");
                 var classes = error.attr("class");
